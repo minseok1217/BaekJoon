9a28a3e57b964fea9e8f4932466095a9bd5b1324
minseok1217 <ping5432121@gmail.com>
2023-11-08 16:19:28 +0900

    solve 2753

---
+++ src/Question2753.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Question2753 {
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        if(a % 4 ==0 && a % 100 != 0){
+            System.out.println(1);
+        }
+        else if(a % 400 == 0){
+            System.out.println(1);
+        }
+        else{
+            System.out.println(0);
+        }
+    }
+}
