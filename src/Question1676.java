5c76ea470c0ba57779b46ee65a16b16fbaa0f778
minseok1217 <ping5432121@gmail.com>
2023-12-09 15:20:28 +0900

    solve problem about simple algorithm

---
+++ src/Question1676.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Question1676{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(n/5+n/25+n/125);
+    }
+}
