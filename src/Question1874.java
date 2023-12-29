4902d7a4a4c1c1cdbc5c3044dfe7e65c2df90dd2
minseok1217 <ping5432121@gmail.com>
2023-12-29 18:43:32 +0900

    modify code because class problem

--- src/Question1874.java
+++ src/Question1874.java
@@ -11,22 +11,17 @@ public class Question1874 {
         StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
         Stack<Integer> stack = new Stack<>();
-        // Stack<Integer> ex = new Stack<>();
-        // StringTokenizer st = new StringTokenizer(br.readLine());
         Queue<Integer> queue = new LinkedList<>();
-        Queue<Integer> answer = new LinkedList<>();
         for(int i = 0; i < n; i++){
             queue.add(Integer.parseInt(br.readLine()));
         }
-        // int num = queue.peek();
         int i = 1;
-        int check = 0;
         while(!queue.isEmpty()){
             stack.add(i);
             sb.append("+\n");
             while(true){
                 if(stack.isEmpty() || queue.isEmpty()) break;
-                if(stack.peek() == queue.peek()){
+                if(stack.peek().equals(queue.peek())){
                     sb.append("-\n");
                     stack.pop();
                     queue.poll();
@@ -44,11 +39,11 @@ public class Question1874 {
             }
         }
         if(queue.isEmpty()){
-            sb.deleteCharAt(sb.length() - 1);
-            System.out.println(sb);
+            // sb.deleteCharAt(sb.length() - 1);
+            System.out.print(sb);
         }
         else{
-            System.out.println("no");
+            System.out.println("NO");
         }
     }
 }
